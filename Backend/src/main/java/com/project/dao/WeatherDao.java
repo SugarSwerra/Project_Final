571c40a2f40c223cc2e34673c90e2b5b3506e49e
SugarSwerra <angeloserra05@gmail.com>
2024-07-19 18:36:05 +0200

    Commit Parziale

--- Backend/src/main/java/com/project/dao/WeatherDao.java
+++
@@ -1,9 +0,0 @@
-package com.project.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.project.model.City;
-
-public interface WeatherDao extends CrudRepository<City, Integer> {
-
-}
